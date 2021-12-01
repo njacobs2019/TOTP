e10b8831fb67c28844e6cdb1cfd92c076c0773a4
njacobs2019 <59352054+njacobs2019@users.noreply.github.com>
2021-12-01 11:34:01 -0500

    Initial version

--- LinkedList.java
+++ LinkedList.java
@@ -12,18 +12,35 @@ class LinkedList{
 
 	// Adds a user to the list
 	public void add(User input){
-		;
+		if head==null{
+			this.head=input;
+			this.tail=input;
+		}
+		else{
+			this.tail.next = input;
+			this.tail = this.tail.next;
+		}
 	}
 
 	// Returns a user object after searching by username
 	// else, returns null
 	public User search(String username){
-		;
+		for(User temp=head; temp!=null; temp=temp.next){
+			if temp.getUsername().equals(username){
+				return temp;
+			}
+		}
+		return null;
 	}
 
 	// Converts the list to a string for printing and debugging
-	// Might need to declare overloading/overriding???
 	public String toString(){
-		;
+		String out = "";
+		for(User temp=head; temp!=null; temp=temp.next){
+			out += "[";
+			out += temp.toString;
+			out += "]";
+		}
+		return out;
 	}
 }
