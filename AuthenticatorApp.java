b99731d563424792aea0ddd3615b337052686964
Nate Roberts <naterob315@gmail.com>
2021-12-01 14:01:55 -0500

    Update AuthenticatorApp.java

--- AuthenticatorApp.java
+++ AuthenticatorApp.java
@@ -1,12 +1,29 @@
 //  This class simulates the Google Authenticator app that runs on someone's phone
+import java.uitl.Scanner;
 
 class AuthenticatorApp(){
+
+private static myApp = new App("people.csv");
+
+public void UsernameInput(){
+	Scanner s = new Scanner(System.in);
+	// ask the user for their username
+	System.out.print("Username: ");
+	String nameInput = s.nextline();
+	System.out.print("\n");
+	// print off their codes for each App instance
+	returnCode = getCode(nameInput);
+	s.close();
+}
 	public static void main(String[] args){
-
-		// ask the user for their username
-
-		// print off their codes for each App instance
 		// have user input loop
+		Scanner scan = new Scanner(System.in);
+		String returnCode = null;
+		UsernameInput();
+		While(returnCode = null){
+			
+			UsernameInput();
+		}
 		//   Print off latest codes, else quit the program (Y/N)
 
 	}
