9bc59e02433cb8f4eabb4dc22a7768b129391dfe
Nate Roberts <naterob315@gmail.com>
2021-12-10 11:30:10 -0500

    Comments

--- AuthenticatorApp.java
+++ AuthenticatorApp.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 class AuthenticatorApp{
-
+//Calls App class and creates App variable to store all the usernames from the storage file
 private static App myApp = new App("people.csv");
 
 	public static void main(String[] args){
@@ -20,22 +20,29 @@ private static App myApp = new App("people.csv");
 		if(returnCode != null){
 			System.out.println(returnCode);
 		}
+		//While loop is entered if user inputs an incorrect username
 		while(returnCode == null && retry.equals("Y")){
-			System.out.print("invalid Input. Type Y to try again or N to exit. ");
+			//Asks user if they would like to try signing in again
+			System.out.print("Invalid Input. Type Y to try again or N to exit. ");
 			retry = s.nextLine();
 			System.out.print("\n");
+			//Asks again for username if user enters Y
 			if(retry.equals("Y")){
 				System.out.print("Username: ");
 				nameInput = s.nextLine();
 				System.out.print("\n");
 				returnCode = myApp.getCode(nameInput);
+				//Prints code if user answers correctly
 				if(returnCode != null){
 					System.out.println(returnCode);
 				}
+				//If user answers incorrectly again then return code = null which returns to the beginning of the while loop
 			}
+			//Exits program if user enters N
 			else if(retry.equals("N")){
 				return;
 			}
+			//Returns to beginning of while loop if user inputs an invalid answer to try again question
 			else{
 				returnCode = null;
 				retry = "Y";
